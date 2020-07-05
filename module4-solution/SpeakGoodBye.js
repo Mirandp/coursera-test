017f2be990e3cc12d81706f54971449a8cff5f66
mirandp <56211486+Mirandp@users.noreply.github.com>
2020-07-05 17:48:08 -0400

    module4-solution/speakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -11,7 +11,7 @@
 // var byeSpeaker =
 
 var byeSpeaker = {};
-byeSpeaker.speak = function () {
+byeSpeaker.speak = function speak(name) {
 	console.log ("Good Bye " + name)
 }
 
@@ -21,11 +21,10 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + "Good Bye " + name);
-} 
 
-speak();
+function speak(name) {
+  console.log(speakWord + byeSpeaker);
+} 
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
